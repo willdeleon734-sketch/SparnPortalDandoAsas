e982b8582909e55707c716055c04be96d94f446a
willdeleon734-sketch <willdeleon734@gmail.com>
2026-09-20 16:05:55 +0000

    Corrige Acesso Beta e endpoint PIX

--- sparn-config.js
+++ sparn-config.js
@@ -1,7 +1,6 @@
 window.SPARN_CONFIG={
-  supabaseUrl:"https://jnuzmaorgbthngodlvgc.supabase.co",
-  supabaseAnonKey:"sb_publishable_VMTCJ_F8MxyZwTK9UDxY-A_uAme3-mj",
-  createPixEndpoint:"https://jnuzmaorgbthngodlvgc.supabase.co/functions/v1/create-pix",
-  planPrice:11.11,
-  planName:"Acesso Beta"
+ supabaseUrl:"https://jnuzmaorgbthngodlvgc.supabase.co",
+ supabaseAnonKey:"sb_publishable_VMTCJ_F8MxyZwTK9UDxY-A_uAme3-mj",
+ createPixEndpoint:"COLE_AQUI_URL_EDGE_FUNCTION_CREATE_PIX",
+ planPrice:11.11, planName:"Acesso Beta"
 };
